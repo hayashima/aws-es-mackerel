f70582577737bbbf4c0573c117549ea90b30dda0
Ryo Sumasu <ryo.sms@gmail.com>
2018-02-26 17:48:13 +0900

    Mod script to kick lambda function

--- kick.sh
+++ kick.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-AWS_REGION=ap-northeast-1 \
-    sam local generate-event schedule \
-    | sam local invoke --template src/template.yaml --env-vars env.json
+sam local generate-event schedule | sam local invoke PostEsMetrics -t src/template.yaml -n env.json
